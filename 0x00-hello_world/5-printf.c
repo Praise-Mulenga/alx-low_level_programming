5e83c8e112de3ac6f7a7fd1e20a71a95220e72b2
Praise Mulenga <praisemulengas@gmail.com>
2023-07-15 05:30:05 -0700

    Hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
  * main - Entry point
- *
+ * Description: 'Prints a string on a line to the console'
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n" );
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
