b34373eab72373cd58389c5ba3f6351097958889
Praise Mulenga <praisemulengas@gmail.com>
2023-07-15 05:17:59 -0700

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
  * main - Entry point
- *
+ * Description: 'Prints a string to the console'
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzl");
 	return (0);
